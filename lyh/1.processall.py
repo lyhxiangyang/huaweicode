7f3008f7fc69de756536fd1976e1c20b304c0e22
lyh <lyhxiangyang@gmail.com>
2021-08-24 15:59:45 +0800

    使用判空函数

--- lyh/1.processall.py
+++ lyh/1.processall.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 
 from Classifiers.ModelTrain import model_train
-from utils.DataFrameOperation import mergeDataFrames, divedeDataFrameByFaultFlag
+from utils.DataFrameOperation import mergeDataFrames, divedeDataFrameByFaultFlag, isEmptyInDataFrame
 from utils.DefineData import WINDOWS_SIZE, MODEL_TYPE
 from utils.FeatureExtraction import featureExtraction
 from utils.FeatureSelection import getUsefulFeatureFromAllDataFrames
@@ -28,6 +28,9 @@ if __name__ == "__main__":
     if err:
         print("数据合并失败")
         exit(1)
+    if isEmptyInDataFrame(mergedPd):
+        print("1. 数据为空")
+        exit(1)
 
     # 2. 根据错误码进行分割得到一个错误码: dataFrame的结构字典结构
     print("2. 数据分割中".center(40, "*"))
@@ -38,12 +41,18 @@ if __name__ == "__main__":
     # 显示一些基本信息
     for i in dictPds:
         print(i, dictPds[i].shape)
+        if isEmptyInDataFrame(dictPds[i]):
+            print("2. 数据为空")
+            exit(1)
 
     # 3. 得到包含只包含某些特征错误码的表格 并且特征提取
     print("3. 特征提取中".center(40, "*"))
     normalPD, err = featureExtraction(dictPds[0], windowSize=WINDOWS_SIZE)
     if err:
         print("特征提取失败")
+        exit(1)
+    if isEmptyInDataFrame(normalPD):
+        print("3. 数据为空")
         exit(1)
     abnormalPD = []
     print("输出特征提取之后，各个错误码对应的数据条数")
@@ -55,6 +64,9 @@ if __name__ == "__main__":
         if err:
             print("abnormal 特征提取失败")
             exit(1)
+        if isEmptyInDataFrame(tpd):
+            print("数据为空")
+            exit(1)
         print("{}: {}".format(i, tpd.shape))
         abnormalPD.append(tpd)
 
@@ -63,6 +75,9 @@ if __name__ == "__main__":
     allUserfulePD, err  = getUsefulFeatureFromAllDataFrames(normalpd=normalPD, abnormalpd=abnormalPD)
     if err:
         print("特征选择失败")
+        exit(1)
+    if isEmptyInDataFrame(allUserfulePD):
+        print("数据为空")
         exit(1)
     print("特征选择之后shape：", allUserfulePD.shape)
     allUserfulePD.to_csv("tmp/4_UsefulFeatures.csv", index=False)
