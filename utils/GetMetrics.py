624c1b248fd6068044d3710261e64ef278c39582
gimsen <1476027061@qq.com>
2021-08-27 20:30:24 +0800

    Add function to get statistical metrics

---
+++ utils/GetMetrics.py
@@ -0,0 +1,27 @@
+def get_metrics(x, y, label):
+    """
+    Get Statistical metrics
+    :param x: Label list of samples
+    :param y: Predicted label list
+    :param label: Label column name
+    :return: tp, fp, fn and tn
+    """
+    true_pos, true_neg = 0, 0
+    false_pos, false_neg = 0, 0
+    for i in range(len(x)):
+        if x[i] == label:
+            if x[i] == y[i]:
+                true_pos += 1
+            else:
+                false_pos += 1
+        else:
+            if x[i] == y[i]:
+                true_neg += 1
+            else:
+                false_neg += 1
+    metrics = {'tp': true_pos, 'tn': true_neg, 'fp': false_pos, 'fn': false_neg}
+    return metrics
+
+    # precision = float('nan') if true_pos + false_pos == 0 else true_pos / (true_pos + false_pos)
+    # recall = float('nan') if true_pos + false_neg == 0 else true_pos / (true_pos + false_neg)
+    # accuracy = (true_pos + true_neg) / len(x)
