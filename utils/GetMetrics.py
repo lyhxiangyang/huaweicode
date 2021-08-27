54c4fdd179a5c7bbea6c945b75e62c9542207c41
gimsen <1476027061@qq.com>
2021-08-27 22:32:28 +0800

    Modify GetMetrics function

--- utils/GetMetrics.py
+++ utils/GetMetrics.py
@@ -1,27 +1,48 @@
-def get_metrics(x, y, label):
+
+from typing import List
+
+"""
+# --函数功能：
+传入实际值列表和预测值列表，输出准确率、召回率等信息
+
+返回值：
+TP: 将正样本预测为正样本的数量
+FN：将正样本预测为负样本的数量
+FP：将负样本预测为正样本的数量
+TN：将负样本预测为副样本的数量
+precision: TP / (TP + FP)   正确预测正样本 占 全部预测正样本的比值   精确率
+recall: TP / (TP + FN) 正确预测正样本 占 正样本总数的比值           召回率
+accuracy: 准确率(accuracy) = 预测对的/所有 = (TP+TN)/(TP+FN+FP+TN) = 70%
+
+
+"""
+
+
+def get_metrics(reallist: List, prelist: List, label: int):
     """
     Get Statistical metrics
-    :param x: Label list of samples
-    :param y: Predicted label list
-    :param label: Label column name
-    :return: tp, fp, fn and tn
+    :param reallist: Label list of samples
+    :param prelist: Predicted label list
+    :param label: Label to be selected
+    :return: tp, fp, fn and tn etc
     """
     true_pos, true_neg = 0, 0
     false_pos, false_neg = 0, 0
-    for i in range(len(x)):
-        if x[i] == label:
-            if x[i] == y[i]:
+    for i in range(len(reallist)):
+        if prelist[i] == label:
+            if reallist[i] == prelist[i]:
                 true_pos += 1
             else:
                 false_pos += 1
         else:
-            if x[i] == y[i]:
+            if reallist[i] != label:
                 true_neg += 1
             else:
                 false_neg += 1
-    metrics = {'tp': true_pos, 'tn': true_neg, 'fp': false_pos, 'fn': false_neg}
+    precision = float('nan') if true_pos + false_pos == 0 else true_pos / (true_pos + false_pos)
+    recall = float('nan') if true_pos + false_neg == 0 else true_pos / (true_pos + false_neg)
+    accuracy = (true_pos + true_neg) / len(reallist)
+    metrics = dict(tp=true_pos, tn=true_neg, fp=false_pos, fn=false_neg, precision=precision, recall=recall,
+                   accuracy=accuracy)
     return metrics
 
-    # precision = float('nan') if true_pos + false_pos == 0 else true_pos / (true_pos + false_pos)
-    # recall = float('nan') if true_pos + false_neg == 0 else true_pos / (true_pos + false_neg)
-    # accuracy = (true_pos + true_neg) / len(x)
