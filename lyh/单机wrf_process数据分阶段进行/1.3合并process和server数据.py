aedac840b9d1b2b691a4dea64333689e1560d5fa
lyh <lyhxiangyang@gmail.com>
2021-09-02 05:37:32 +0800

    添加训练完整过程

--- lyh/单机wrf_process数据分阶段进行/1.3合并process和server数据.py
+++ lyh/单机wrf_process数据分阶段进行/1.3合并process和server数据.py
@@ -2,8 +2,9 @@
 这一步的作用就是将process的数据和server的数据结合起来
 """
 
-savepathfile1_1 = "tmp\\tmp\\wrf_single_process\\1.1\\mergePD_After.csv"
+savepathfile1_1 = "tmp\\wrf_single_process\\1.1\\mergePD_After.csv"
 savepathfile1_2 = "tmp\\wrf_single_process\\1.2\\1.2_serverData_mergePd.csv"
 
 if __name__ == "__main__":
     # 验证一波
+    pass
