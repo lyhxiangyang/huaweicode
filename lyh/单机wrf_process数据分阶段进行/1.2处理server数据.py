6666e8fbadc2867cb168ce82a9918f25b8f0f50d
lyh <lyhxiangyang@gmail.com>
2021-09-02 02:43:36 +0800

    将server数据和process数据结合起来失败
    添加训练完整过程
    
    添加process正常文件
    
    开始进行结合server和process数据
    
    解决SettingwithCopyWarning
    
    添加参数说明
    
    如果进行copy会出错，尝试不进行copy
    
    1.2中保存路径出错
    
    修改1.2中某个返回值bug
    
    处理server数据的数据
    
    将tuple转化为list
    
    判断是否两个特征是否有交集
    
    能够自动生成保存数据的路径
    
    修改了部分不存在的路径
    
    能够将所有的FlagFaulty标记并且合成一个集合

---
+++ lyh/单机wrf_process数据分阶段进行/1.2处理server数据.py
@@ -0,0 +1,59 @@
+"""
+此文件的作用是将server数据进行合并
+"""
+import os
+
+import pandas as pd
+
+from utils.DataFrameOperation import isEmptyInDataFrame, subtractFirstLineFromDataFrame, mergeDataFrames
+from utils.DefineData import FAULT_FLAG, TIME_COLUMN_NAME
+
+AllCSVFiles = [
+    "D:\\HuaweiMachine\\测试数据\\wrfrst_normal_e5\\result\\normal_single\\wrfrst_e5-43_server.csv",
+    "D:\\HuaweiMachine\\测试数据\\wrfrst单机版e5\\wrfrst单机版e5\\wrfrst-7.16-1\\wrf_rst_e5-43_server.csv",
+    "D:\\HuaweiMachine\\测试数据\\wrfrst单机版e5\\wrfrst单机版e5\\wrfrst-7.17-2\\wrf_rst_e5-43_server.csv"
+]
+savepath = "tmp\\wrf_single_process\\1.2\\"
+
+if __name__ == "__main__":
+    # 先将路径自动生成
+    if not os.path.exists(savepath):
+        os.makedirs(savepath)
+
+    ####################################################################################################################
+    print("server数据合并中".center(40, "*"))
+    tallPds = [pd.read_csv(ipath) for ipath in AllCSVFiles]
+    # ==判断是否存在空
+    for ipds in tallPds:
+        print("shape: {}".format(ipds.shape))
+        if isEmptyInDataFrame(ipds):
+            print("打开的csv文件中， 有空值")
+            exit(1)
+
+    # == 将每一个Pd中的数据都减去第一行
+    allcolumns = list(tallPds[0].columns)
+    allcolumns.remove(FAULT_FLAG)
+    allcolumns.remove(TIME_COLUMN_NAME)
+    allPds = []
+    for ipd in tallPds:
+        tpd, err = subtractFirstLineFromDataFrame(df=ipd, columns=allcolumns)
+        if err:
+            print("在处理server数据中，减去第一行出现问题")
+            exit(1)
+        allPds.append(tpd)
+
+    # == 将多个文件进行合并
+    meregdPd, err = mergeDataFrames(allPds)
+    if err:
+        print("数据合并失败")
+        exit(1)
+    print("合并之后文件大小：", meregdPd.shape)
+    if isEmptyInDataFrame(meregdPd):
+        print("合并之后的文件存在空值")
+        exit(1)
+    print("文件中没有空值")
+
+    ####################################################################################################################
+    # 将文件进行保存
+    tpath = os.path.join(savepath, "1.2_serverData_mergePd.csv")
+    meregdPd.to_csv(tpath, index=False)
---
+++ lyh/单机wrf_process数据分阶段进行/6.1模型预测.py
@@ -0,0 +1,136 @@
+"""
+这个文件的主要作用是指定新的文件，然后进程步骤1，2，3，4，5
+输入： 指定文件
+输出：模型文件
+"""
+import os
+from typing import Dict
+
+from Classifiers.ModelPred import select_and_pred
+from utils.DefineData import SaveModelPath, FAULT_FLAG
+import pandas as pd
+import numpy as np
+
+from Classifiers.ModelTrain import model_train
+from utils.DataFrameOperation import mergeDataFrames, divedeDataFrameByFaultFlag, isEmptyInDataFrame
+from utils.DefineData import WINDOWS_SIZE, MODEL_TYPE
+from utils.FeatureExtraction import featureExtraction
+from utils.FeatureSelection import getUsefulFeatureFromAllDataFrames
+from utils.GetMetrics import get_metrics
+
+"""
+使用测试数据中的单机server来预测《数据修订版》中的单机数据
+"""
+
+modelpath = os.path.join(SaveModelPath, "1")
+
+AllCSVFiles = [
+    "D:\\HuaweiMachine\\数据修订版\\单机整合数据\\wrf-1km-单机数据整合版\\wrf_1km_160_server.csv",
+    "D:\\HuaweiMachine\\数据修订版\\单机整合数据\\wrf-3km-单机数据整合版\\wrf_3km_160_server.csv",
+    "D:\\HuaweiMachine\\数据修订版\\单机整合数据\\wrf-9km-单机数据整合版\\wrf_9km_160_server.csv",
+]
+saverespath = "tmp\\informations"
+
+if __name__ == "__main__":
+    ####################################################################################################################
+    print("1. 数据合并进行中".center(40, "*"))
+    allPds = [pd.read_csv(ipath) for ipath in AllCSVFiles]
+
+    # 进行判空操作
+    for ipd in allPds:
+        if isEmptyInDataFrame(ipd):
+            print("数据有空")
+            exit(1)
+
+    # 合并操作
+    mergedPd, err = mergeDataFrames(allPds)
+    if err:
+        print("数据合并失败")
+        exit(1)
+    if isEmptyInDataFrame(mergedPd):
+        print("合并之后的DataFrame有NAN")
+        exit(1)
+
+    ####################################################################################################################
+    print("2. 数据分割中".center(40, "*"))
+    dictPds: Dict
+    dictPds, err = divedeDataFrameByFaultFlag(mergedPd)
+    if err:
+        print("数据分割失败")
+        exit(1)
+    for i in dictPds.keys():
+        print(i, dictPds[i].shape)
+        if isEmptyInDataFrame(dictPds[i]):
+            print("错误码{}数据有空".format(i))
+            exit(1)
+
+    ####################################################################################################################
+    print("3. 特征提取中".center(40, "*"))
+    normalPD, err = featureExtraction(dictPds[0], windowSize=WINDOWS_SIZE)
+    if err:
+        print("特征提取失败")
+        exit(1)
+
+    abnormalPD = []
+    # 为了选择模型中存在的flag
+    tpath = "tmp\\single\\4\\alluserful.csv"
+    tpd = pd.read_csv(tpath)
+    ts = set(tpd[FAULT_FLAG])
+
+    print("错误码  数据条数".center(20, "="))
+    for i in dictPds.keys():
+        print("{}: {}".format(i, dictPds[i].shape))
+        if i == 0 or i not in ts:
+            continue
+        tpd, err = featureExtraction(dictPds[i], windowSize=WINDOWS_SIZE)
+        print("{}: {}".format(i, tpd.shape))
+        if err:
+            print("abnormal 特征提取失败")
+            exit(1)
+        if isEmptyInDataFrame(tpd):
+            print("错误码{}: 数据有空".format(i))
+            exit(1)
+        abnormalPD.append(tpd)
+    print("错误码  数据条数 结束".center(20, "="))
+
+    ####################################################################################################################
+    print("4. 将数据进行合并")
+    dfs = [normalPD]
+    dfs.extend(abnormalPD)
+    mergedPd, err = mergeDataFrames(dfs)
+    mergedPd: pd.DataFrame
+    if err:
+        print("步骤4中数据合并失败")
+        exit(1)
+    # 将mergedPd进行输出
+    mergedPd.to_csv("tmp\\1.input.csv", index=False)
+
+    ####################################################################################################################
+    print("调用1中的模型进行预测".center(40, "*"))
+    reallist = mergedPd[FAULT_FLAG]
+    #  包含准确率、召回率、精确率以及对应模型的的字典
+    tDic = {}
+    for itype in MODEL_TYPE:
+        prelist = select_and_pred(mergedPd, model_type=itype, saved_model_path=modelpath)
+        anumber = len(prelist)
+        rightnumber = len([i for i in range(0, len(prelist)) if prelist[i] == reallist[i]])
+        print("{}: 一共预测{}数据，其中预测正确{}数量, 正确率{}".format(itype, anumber, rightnumber, rightnumber / anumber))
+        tallFault = sorted(list(set(reallist)))
+        for i in tallFault:
+            if i not in tDic.keys():
+                tDic[i] = {}
+            tmetrics = get_metrics(reallist, prelist, i)
+            # 将数据进行保存
+            tDic[i]["accuracy_" + itype] = tmetrics["accuracy"]
+            tDic[i]["precision_" + itype] = tmetrics["precision"]
+            tDic[i]["recall_" + itype] = tmetrics["recall"]
+
+    # 将数据进行输出并保存
+    if not os.path.exists(saverespath):
+        os.makedirs(saverespath)
+    itpd = pd.DataFrame(data=tDic).T
+    print(itpd)
+    itpd.to_csv(os.path.join(saverespath, "单机6_1预测数据统计信息.csv"))
+    print("=========================")
+    print("输出信息->", os.path.join(saverespath, "单机6_1预测数据统计信息.csv"))
+    print("模型预测结束")
