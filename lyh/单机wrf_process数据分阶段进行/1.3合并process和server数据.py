830e327c2cf6103a7c950089783f5ff1fa0894e1
lyh <lyhxiangyang@gmail.com>
2021-09-02 04:52:04 +0800

    修改路径问题

--- lyh/单机wrf_process数据分阶段进行/1.3合并process和server数据.py
+++ lyh/单机wrf_process数据分阶段进行/1.3合并process和server数据.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 from utils.DefineData import TIME_COLUMN_NAME
 
-savepathfile1_1 = "tmp\\tmp\\wrf_single_process\\1.1\\mergePD_After.csv"
+savepathfile1_1 = "tmp\\wrf_single_process\\1.1\\mergePD_After.csv"
 savepathfile1_2 = "tmp\\wrf_single_process\\1.2\\1.2_serverData_mergePd.csv"
 
 if __name__ == "__main__":
