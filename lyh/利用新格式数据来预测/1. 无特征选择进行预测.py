281877bbcb6c2008fd48ae359d182a7ab95d138f
lyh <lyhxiangyang@gmail.com>
2021-09-27 22:08:08 +0800

    bug

--- lyh/利用新格式数据来预测/1. 无特征选择进行预测.py
+++ lyh/利用新格式数据来预测/1. 无特征选择进行预测.py
@@ -19,8 +19,10 @@ trainDataPath = [
     "tmp/tData/多机-E5-process-server-1KM/所有process错误码信息/0.csv",
     "tmp/tData/多机-E5-process-server-1KM/所有process错误码信息/15.csv",
     "tmp/tData/多机-E5-process-server-1KM/所有process错误码信息/65.csv",
+    "tmp/tData/多机-红区-process-server-1KM/所有process错误码信息/0.csv",
 ]
 testDataPath = [
+    "tmp/tData/多机-红区-process-server-1KM/所有process错误码信息/0.csv",
     "tmp/tData/多机-红区-process-server-1KM/所有process错误码信息/15.csv",
     "tmp/tData/多机-红区-process-server-1KM/所有process错误码信息/65.csv",
 ]
