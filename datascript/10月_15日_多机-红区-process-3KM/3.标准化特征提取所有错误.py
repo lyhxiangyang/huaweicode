a165bb31396f5804c86b089f5d65a39b856b1b71
lyh <lyhxiangyang@gmail.com>
2021-10-18 19:45:58 +0800

    bug

--- datascript/10月_15日_多机-红区-process-3KM/3.标准化特征提取所有错误.py
+++ datascript/10月_15日_多机-红区-process-3KM/3.标准化特征提取所有错误.py
@@ -287,7 +287,7 @@ if __name__ == "__main__":
     spath = "tmp/tData-10-18/多机-红区-process-server-3KM"
     extractedFeaturee = ["load1"]
     # 将所有的标准化数据读取
-    file_time_core_standardPath = "tmp/tData-10-18/多机-红区-process-server-1KM/4.filename-time-core-标准化"
+    file_time_core_standardPath = "tmp/tData-10-18/多机-红区-process-server-3KM/4.filename-time-core-标准化"
     print("读取filename-time-core数据中".center(40, "*"))
     file_time_core_standardDict = readFilename_Time_Core_pdDict(file_time_core_standardPath)
     # 进行特征提取
