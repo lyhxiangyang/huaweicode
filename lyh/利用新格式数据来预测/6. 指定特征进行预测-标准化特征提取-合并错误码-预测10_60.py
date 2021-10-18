a19dcc44257530988b8cf0c58f4f4f85c4bbefd3
lyh <lyhxiangyang@gmail.com>
2021-10-19 00:32:25 +0800

    bug

---
+++ lyh/利用新格式数据来预测/6. 指定特征进行预测-标准化特征提取-合并错误码-预测10_60.py
@@ -0,0 +1,145 @@
+from typing import Union, Any, List
+
+import pandas as pd
+from pandas import DataFrame, Series
+from pandas.io.parsers import TextFileReader
+
+from Classifiers.TrainToTest import ModelTrainAndTest
+from utils.DataFrameOperation import mergeDataFrames
+from utils.DefineData import FAULT_FLAG
+
+# 1KM数据预测
+# trainNormalDataPath = [
+#     "tmp/tData-10-18/多机-E5-process-server-1KM/7.特征提取所有错误/0.csv",
+#     "tmp/tData-10-18/多机-红区-process-server-1KM/7.特征提取所有错误/0.csv",
+# ]
+#
+# trainAbnormalDataPath = [
+#     "tmp/tData-10-18/多机-E5-process-server-1KM/7.特征提取所有错误/13.csv",
+#     "tmp/tData-10-18/多机-E5-process-server-1KM/7.特征提取所有错误/14.csv",
+#     "tmp/tData-10-18/多机-E5-process-server-1KM/7.特征提取所有错误/15.csv",
+# ]
+#
+# testNormalDataPath = [
+#     "tmp/tData-10-18/多机-红区-process-server-1KM/7.特征提取所有错误/0.csv",
+# ]
+#
+# testAbnormalDataPath = [
+#     "tmp/tData-10-18/多机-红区-process-server-1KM/7.特征提取所有错误/13.csv",
+#     "tmp/tData-10-18/多机-红区-process-server-1KM/7.特征提取所有错误/14.csv",
+#     "tmp/tData-10-18/多机-红区-process-server-1KM/7.特征提取所有错误/15.csv",
+# ]
+
+trainNormalDataPath = [
+    "tmp/tData-10-18/多机-E5-process-server-3KM/7.特征提取所有错误/0.csv",
+    "tmp/tData-10-18/多机-红区-process-server-3KM/7.特征提取所有错误/0.csv",
+]
+
+trainAbnormalDataPath = [
+    (10, "tmp/tData-10-18/多机-E5-process-server-3KM/7.特征提取所有错误/13.csv"),
+    (10, "tmp/tData-10-18/多机-E5-process-server-3KM/7.特征提取所有错误/14.csv"),
+    (10, "tmp/tData-10-18/多机-E5-process-server-3KM/7.特征提取所有错误/15.csv"),
+    (60, "tmp/tData-10-18/多机-E5-process-server-3KM/7.特征提取所有错误/63.csv"),
+    (60, "tmp/tData-10-18/多机-E5-process-server-3KM/7.特征提取所有错误/64.csv"),
+    (60, "tmp/tData-10-18/多机-E5-process-server-3KM/7.特征提取所有错误/65.csv"),
+]
+
+testNormalDataPath = [
+    "tmp/tData-10-18/多机-红区-process-server-3KM/7.特征提取所有错误/0.csv",
+]
+
+testAbnormalDataPath = [
+    (10, "tmp/tData-10-18/多机-红区-process-server-3KM/7.特征提取所有错误/13.csv"),
+    (10, "tmp/tData-10-18/多机-红区-process-server-3KM/7.特征提取所有错误/14.csv"),
+    (10, "tmp/tData-10-18/多机-红区-process-server-3KM/7.特征提取所有错误/15.csv"),
+    (60, "tmp/tData-10-18/多机-红区-process-server-3KM/7.特征提取所有错误/63.csv"),
+    (60, "tmp/tData-10-18/多机-红区-process-server-3KM/7.特征提取所有错误/64.csv"),
+    (60, "tmp/tData-10-18/多机-红区-process-server-3KM/7.特征提取所有错误/65.csv"),
+]
+
+
+def get_List_pre_suffix(clist: List[str], prefix: str = "", suffix: str = "") -> List[str]:
+    return [i for i in clist if i.startswith(prefix) and i.endswith(suffix)]
+def get_List_nosuffix(clist: List[str], suffix: str="") -> List[str]:
+    if suffix == "":
+        return clist
+    return [i for i in clist if not i.endswith(suffix)]
+
+
+# 将一个DataFrame的FAULT_FLAG重值为ff
+def setPDfaultFlag(df: pd.DataFrame, ff: int) -> pd.DataFrame:
+    if FAULT_FLAG in df.columns.array:
+        df = df.drop(FAULT_FLAG, axis=1)
+    lengthpd = len(df)
+    ffdict = {FAULT_FLAG: [ff] * lengthpd}
+    tpd = pd.DataFrame(data=ffdict)
+    tpd = pd.concat([df, tpd], axis=1)
+    return tpd
+
+"""
+指定标准化之后并且特征提取的数据， 去除后缀为_diff的特征，对强度为15的数据进行验证
+"""
+if __name__ == "__main__":
+    spath = "tmp/E5多机预测红区-单特征load1-标准化特征提取-合并错误"
+    # trainedPDList: list[Union[Union[TextFileReader, Series, DataFrame, None], Any]] = []
+    # for i in trainDataPath:
+    #     tpd = pd.read_csv(i)
+    #     trainedPDList.append(tpd)
+    # allTrainedPD, err = mergeDataFrames(trainedPDList)
+    # allTrainedPD: pd.DataFrame
+    # if err:
+    #     print("train合并出错")
+    #     exit(1)
+    #
+    # testPDList = []
+    # for i in testDataPath:
+    #     tpd = pd.read_csv(i)
+    #     testPDList.append(tpd)
+    # allTestPD, err = mergeDataFrames(testPDList)
+    # if err:
+    #     print("test合并出错")
+    #     exit(1)
+    #==================================================================读取训练的normal数据
+    trainNormalList = []
+    for i in trainNormalDataPath:
+        tpd = pd.read_csv(i)
+        trainNormalList.append(tpd)
+    #==================================================================读取训练的abnormal数据
+    trainAbnormalList = []
+    for ilabel, ipath in trainAbnormalDataPath:
+        tpd = pd.read_csv(ipath)
+        # 修改list的标签
+        tpd = setPDfaultFlag(tpd, ilabel)
+        trainAbnormalList.append(tpd)
+    #==================================================================读取测试的normal数据
+    testNormalList = []
+    for i in testNormalDataPath:
+        tpd = pd.read_csv(i)
+        testNormalList.append(tpd)
+    #==================================================================读取训练的normal数据
+    testAbnormalList = []
+    for ilabel, ipath in testAbnormalDataPath:
+        tpd = pd.read_csv(ipath)
+        tpd = setPDfaultFlag(tpd, ilabel)
+        testNormalList.append(tpd)
+
+    #==================================================================将所有的训练数据进行合并
+    allTrainedPD, err = mergeDataFrames(trainNormalList + trainAbnormalList)
+    if err:
+        print("训练数据合并失败")
+        exit(1)
+    #==================================================================将所有的测试数据进行合并
+    allTestPD, err = mergeDataFrames(testNormalList + testAbnormalList)
+    if err:
+        print("测试数据合并失败")
+        exit(1)
+
+
+
+    # 获得需要训练的特征
+    allfeatureload1_nosuffix = get_List_nosuffix(list(allTrainedPD.columns.array), suffix="_diff")
+    # allfeatureload1_nosuffix = list(allTrainedPD.columns.array)
+
+    print("选择的特征：{}".format(str(allfeatureload1_nosuffix)))
+    ModelTrainAndTest(allTrainedPD, allTestPD, spath=spath, selectedFeature=allfeatureload1_nosuffix, modelpath="Classifiers/saved_model/tmp_load1_nosuffix")
+
