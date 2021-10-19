62408ea21e18fef740f8541aa91373df4303fc50
lyh <lyhxiangyang@gmail.com>
2021-10-19 14:34:20 +0800

    bug

---
+++ lyh1/1.pre_singlefiles/1.单独预测指定文件的核心文件.py
@@ -0,0 +1,25 @@
+
+"""
+单独指定datascript中步骤3中生成的每个文件每个核心的文件使用现有模型进行预测，预测结果存储在
+"""
+import os.path
+
+import pandas as pd
+
+from Classifiers.TrainToTest import testThree
+
+
+if __name__ == "__main__":
+    spath = "tmp/allpreinformation"
+    rpath = "tmp/tData-10-18/多机-红区-process-server-3KM/6.filename-time-core-标准化-特征提取"
+    prefilename = "wrf_3km_160_process"
+    timeequantum = 0
+    prefilenamecore = 0
+    usemodelpath = "Classifiers/saved_model/tmp_load1_nosuffix"
+
+    # 要预测文件的路径
+    filepath = os.path.join(rpath, prefilename, str(timeequantum), str(prefilenamecore) + ".csv")
+    fileinfosavepath = os.path.join(spath, prefilename, str(timeequantum), str(prefilenamecore))
+    prepd = pd.read_csv(filepath)
+    testThree(prepd, spath=fileinfosavepath, modelpath=usemodelpath)
+
