9cf0c6c82b0427560a33def2d3ce712f5653c766
lyh <lyhxiangyang@gmail.com>
2021-10-22 16:44:44 +0800

    bug

--- utils/DrawDecisionTree.py
+++ utils/DrawDecisionTree.py
@@ -9,7 +9,7 @@ from utils.DefineData import MODEL_TYPE
 
 def plot_tree_structure(depth, modelpath: str):
     # Load the model
-    modelname = os.path.join(modelpath, MODEL_TYPE[0])
+    modelname = os.path.join(modelpath, MODEL_TYPE[0] + ".pkl")
     model = joblib.load(modelname)
     classes = ["Normal", "CPU_ALL"]
     headerfile = os.path.join(modelpath, "header.txt")
